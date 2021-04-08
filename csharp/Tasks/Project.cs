fc808e6ea7b68b0a008a867ec77dd8512e0af4fc
Didier Cauvin <didier.cauvin@mediane.tm.fr>
2021-04-08 15:44:18 +0200

    Ajout liste des taches sur Project

--- csharp/Tasks/Project.cs
+++ csharp/Tasks/Project.cs
@@ -8,11 +8,13 @@ namespace Tasks
 {
     public class Project
     {
-        public Project(string name)
+        public Project(string name, IEnumerable<Task> tasks)
         {
             Name = name;
+            Tasks = tasks.ToList();
         }
 
         public string Name { get; }
+        public IList<Task> Tasks { get; }
     }
 }
