f89d770a2d996f4b89c804c629d85b64db2d6d25
Didier Cauvin <didier.cauvin@mediane.tm.fr>
2021-04-08 15:43:10 +0200

    Ajout du nom sur Project

--- csharp/Tasks/Project.cs
+++ csharp/Tasks/Project.cs
@@ -8,6 +8,11 @@ namespace Tasks
 {
     public class Project
     {
-        
+        public Project(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
     }
 }
