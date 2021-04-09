4e75963237345ea1ace42a6840bc780a79c5d41a
Didier Cauvin <didier.cauvin@mediane.tm.fr>
2021-04-09 11:05:38 +0200

    Ajout Projects CreateTaskCommand

--- csharp/Tasks/CreateTaskCommand.cs
+++ csharp/Tasks/CreateTaskCommand.cs
@@ -10,6 +10,8 @@ namespace Tasks
     {
         public class Handler
         {
+            public Projects Projects { get; }
+
             public void Execute(CreateTaskCommand command)
             {
 
