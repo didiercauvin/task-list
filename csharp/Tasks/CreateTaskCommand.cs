c534bdf5aaa15058c6aff5b1241c2ee16125284c
Didier Cauvin <didier.cauvin@mediane.tm.fr>
2021-04-09 11:01:09 +0200

    Création CreateTaskCommand

---
+++ csharp/Tasks/CreateTaskCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class CreateTaskCommand
+    {
+        public class Handler
+        {
+            public void Execute(CreateTaskCommand command)
+            {
+
+            }
+        }
+    }
+}
