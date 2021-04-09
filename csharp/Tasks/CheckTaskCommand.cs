0860817b51de382a8b61a887c133af76bd6230e9
Didier Cauvin <didier.cauvin@mediane.tm.fr>
2021-04-09 11:18:56 +0200

    Ajout propriété Projects CheckTaskCommand

--- csharp/Tasks/CheckTaskCommand.cs
+++ csharp/Tasks/CheckTaskCommand.cs
@@ -8,12 +8,14 @@ namespace Tasks
 {
     public class CheckTaskCommand
     {
-        public CheckTaskCommand(string taskId)
+        public CheckTaskCommand(Projects projects, string taskId)
         {
             TaskId = taskId;
+            Projects = projects;
         }
 
         public string TaskId { get; }
+        public Projects Projects { get; }
 
         public class Handler
         {
