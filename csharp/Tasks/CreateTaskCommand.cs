2d92e0600f39385f7dd49f7f1143bb1680d33abd
Didier Cauvin <didier.cauvin@mediane.tm.fr>
2021-04-09 11:07:16 +0200

    Add TaskDescription CreateTaskCommand

--- csharp/Tasks/CreateTaskCommand.cs
+++ csharp/Tasks/CreateTaskCommand.cs
@@ -8,10 +8,17 @@ namespace Tasks
 {
     public class CreateTaskCommand
     {
+        public Projects Projects { get; }
+        public string TaskDescription { get; }
+
+        public CreateTaskCommand(Projects projects, string taskDescription)
+        {
+            Projects = projects;
+            TaskDescription = taskDescription;
+        }
+
         public class Handler
         {
-            public Projects Projects { get; }
-
             public void Execute(CreateTaskCommand command)
             {
 
